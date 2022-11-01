2a6d380149e26ab8611572f802bb443d92cb6f05
tecnodiniz <73700400+tecnodiniz@users.noreply.github.com>
2022-11-01 10:02:31 -0300

    4 files modified

--- src/app/components/button.component.ts
+++ src/app/components/button.component.ts
@@ -1,11 +1,15 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'my-button',
   templateUrl: './button.component.html',
+
   styles: ['button{margin-top:10px}'],
 })
 export class ButtonComponent {
- @Input() buttonText:string
-  
+  @Input() buttonText: string;
+
+  getAlert() {
+    alert(this.buttonText);
+  }
 }
